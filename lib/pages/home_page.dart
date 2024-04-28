8bcafa93c06343877488333b8cb5515641d4de34
Victor-chiemerie <victorchiemerie02@gmail.com>
2024-04-28 15:26:22 +0100

    complete drawer functions

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,28 +1,16 @@
-import 'package:chat/auth/auth_service.dart';
+import 'package:chat/components/my_drawer.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  void logout() {
-    // log out
-    final auth = AuthService();
-    auth.signOut();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
-        actions: [
-          IconButton(
-            onPressed: logout,
-            icon: const Icon(Icons.logout),
-          )
-        ],
       ),
-      drawer: Drawer(),
+      drawer: const MyDrawer(),
     );
   }
 }
