59fcf7458002dcbef61e91630fe1f979d70d47ae
Victor-chiemerie <victorchiemerie02@gmail.com>
2024-05-05 09:25:15 +0100

    complete chat app

--- lib/components/my_icon.dart
+++ lib/components/my_icon.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 
 class RoundIconButton extends StatelessWidget {
-    final IconData iconData;
-    final Color backgroundColor;
+  final IconData iconData;
+  final Color backgroundColor;
 
-    const RoundIconButton({required this.iconData, required this.backgroundColor});
+  const RoundIconButton({
+    super.key,
+    required this.iconData,
+    required this.backgroundColor,
+  });
 
-    @override
-    Widget build(BuildContext context) {
-        return Container(
-            decoration: BoxDecoration(
-                color: backgroundColor,
-                shape: BoxShape.circle,
-            ),
-            child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Icon(
-                    iconData,
-                    color: Colors.white, // Set icon color (usually white for contrast)
-                ),
-            ),
-        );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        shape: BoxShape.circle,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(
+          iconData,
+          color: Colors.white, // Set icon color (usually white for contrast)
+        ),
+      ),
+    );
+  }
 }
